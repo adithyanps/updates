9bae8f26f972d2471ca6905b4b8cb774478d4979
adithyanps <adithyanps3@gmail.com>
2017-11-13 03:39:21 -0800

    test1

--- test/b.py
+++ test/b.py
@@ -6,12 +6,10 @@ class Complex:
  def __str__(self):
   return str(self.real)+ '+i' + str(self.imag)
  def add(self, other):
-  f=Complex(self.real + other.real,self.imag + other.imag)
-  c=Complex(f.real,f.imag)
+  c=Complex(self.real + other.real,self.imag + other.imag)
   return c
  def mul(self, other):
-   g=Complex(self.real*other.real - self.imag*other.imag,self.imag*other.real + self.real*other.imag)
-   d=Complex(g.real,g.imag)
+   d=Complex(self.real*other.real - self.imag*other.imag,self.imag*other.real + self.real*other.imag)
    return d
 a=Complex(1,2)
 b=Complex(3,4)
@@ -19,3 +17,4 @@ c=a.add(b)
 d=a.mul(b)
 print c
 print d
+
